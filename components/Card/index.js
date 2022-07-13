f68c7c8d9f4dfa1bdc30577f36cde74d64dcaebb
Frans Jesky <fransjesky@hotmail.com>
2022-07-13 23:57:26 +0700

    feature(card): created card story boilerplate

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
-function Card({ image }) {
-  return <Box>Card</Box>;
+function Card({ image, title }) {
+  const styleSheets = {
+    container: {
+      width: '15rem',
+      height: '20rem',
+      borderRadius: '0.25rem',
+      backgroundColor: '#121212',
+      overflow: 'hidden',
+      position: 'relative',
+    },
+    titleText: {
+      color: '#ffffff',
+    },
+  };
+
+  return (
+    <Box sx={styleSheets.container}>
+      <Image src={image} layout='fill' objectFit='cover' quality={100} />
+      <Typography sx={styleSheets.titleText}>{title}</Typography>
+    </Box>
+  );
 }
 
 Card.propTypes = {
---
+++ stories/Card.stories.js
@@ -0,0 +1,8 @@
+import Card from '../components/Card';
+
+export default {
+  title: 'Card',
+  component: Card,
+};
+
+export const Default = () => <Card image='/assets/sample.jpg' />;
