4de11528636461f7d282973d7979c042cdfa6c6d
Frans Jesky <fransjesky@hotmail.com>
2022-07-14 09:16:48 +0700

    feature(card): created default card component

--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -5,4 +5,10 @@ export default {
   component: Card,
 };
 
-export const Default = () => <Card image='/assets/sample.jpg' />;
+export const Default = () => (
+  <Card
+    image='/assets/sample.jpg'
+    title='Card Default'
+    desc='This is an example for default variant of card component'
+  />
+);
