43a1e2ad816d754133e5865e0bb9aafe327c1f13
Frans Jesky <fransjesky@hotmail.com>
2022-07-15 15:43:21 +0700

    feature(card): redesigned card component

--- components/Card/index.js
+++ components/Card/index.js
@@ -2,33 +2,29 @@ import React from 'react';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@mui/material';
-
-// icons
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-function Card({ image, likes = '0', title, desc }) {
+function Card({ image, title, copyright }) {
   const styleSheets = {
     container: {
-      width: '15rem',
+      width: '12.5rem',
       height: '20rem',
-      borderRadius: '0.25rem',
-      backgroundColor: '#121212',
       overflow: 'hidden',
       position: 'relative',
+      transition: 'all 0.3s ease',
       '&:after': {
         content: '""',
         position: 'absolute',
-        top: '50%',
+        top: '80%',
         left: '0',
         right: '0',
         bottom: '0',
         background: 'linear-gradient(to bottom, transparent, #000000)',
       },
-    },
-    iconContainer: {
-      padding: '0.5rem',
-      position: 'absolute',
-      top: '0',
-      zIndex: '1',
+      '&:hover': {
+        cursor: 'pointer',
+        transform: 'translateY(-4px)',
+        boxShadow:
+          '0 1rem 1.875rem rgba(0, 0, 0, 0.25), 0 0.625rem 0.625rem rgba(0, 0, 0, 0.25)',
+      },
     },
     textContainer: {
       width: '100%',
@@ -37,21 +33,12 @@ function Card({ image, likes = '0', title, desc }) {
       bottom: '0',
       zIndex: '1',
     },
-    statsContainer: {
-      color: '#ffffff',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    statsText: {
-      fontSize: '0.75rem',
-    },
     titleText: {
       color: '#ffffff',
       fontSize: '1rem',
       fontWeight: '700',
     },
-    descText: {
+    copyrightText: {
       color: '#e0e0e0',
       fontSize: '0.75rem',
       fontWeight: '400',
@@ -67,17 +54,9 @@ function Card({ image, likes = '0', title, desc }) {
         objectFit='cover'
         quality={100}
       />
-      <Box sx={styleSheets.iconContainer}>
-        <Box sx={styleSheets.statsContainer}>
-          <FavoriteBorderIcon
-            sx={{ fontSize: '1rem', marginRight: '0.25rem' }}
-          />
-          <Typography sx={styleSheets.statsText}>{likes}</Typography>
-        </Box>
-      </Box>
       <Box sx={styleSheets.textContainer}>
         <Typography sx={styleSheets.titleText}>{title}</Typography>
-        <Typography sx={styleSheets.descText}>{desc}</Typography>
+        <Typography sx={styleSheets.copyrightText}>{copyright}</Typography>
       </Box>
     </Box>
   );
@@ -86,7 +65,7 @@ function Card({ image, likes = '0', title, desc }) {
 Card.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
+  copyright: PropTypes.string.isRequired,
 };
 
 export default Card;
